6afb1dd19486aa5c771246b254a7b64bc4372130
am-gooru <ashish@gooru.org>
2016-05-12 16:36:43 +0530

    Send the event instead of publishing it

--- src/main/java/org/gooru/nucleus/handlers/taxonomy/bootstrap/TaxonomyVerticle.java
+++ src/main/java/org/gooru/nucleus/handlers/taxonomy/bootstrap/TaxonomyVerticle.java
@@ -49,7 +49,7 @@ public class TaxonomyVerticle extends AbstractVerticle {
                             } else {
                                 LOGGER.warn("Invalid session token received");
                             }
-                            eb.publish(MessagebusEndpoints.MBEP_EVENT, eventData);
+                            eb.send(MessagebusEndpoints.MBEP_EVENT, eventData);
                         }
                     });
                 }).completionHandler(result -> {
