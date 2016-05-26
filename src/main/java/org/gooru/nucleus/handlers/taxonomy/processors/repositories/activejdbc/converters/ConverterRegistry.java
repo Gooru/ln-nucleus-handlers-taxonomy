667ae8765b41f06366e2691c94b2dacb874c1e29
Ashish Mishra <ashish@gooru.org>
2016-05-26 10:00:22 +0530

    Clean up and removal of unused classes

--- src/main/java/org/gooru/nucleus/handlers/taxonomy/processors/repositories/activejdbc/converters/ConverterRegistry.java
+++
@@ -1,5 +0,0 @@
-package org.gooru.nucleus.handlers.taxonomy.processors.repositories.activejdbc.converters;
-
-public interface ConverterRegistry {
-    FieldConverter lookupConverter(String fieldName);
-}
