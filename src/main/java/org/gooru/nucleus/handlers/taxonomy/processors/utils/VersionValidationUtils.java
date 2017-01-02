bec51b1bf5f4b478061e0828a6b2ad44ce389d5b
am-gooru <ashish@gooru.org>
2017-01-02 19:22:40 +0530

    Provide infra structure so that handlers can selectively deprecate the versions for specific APIs while the same global versions may be supported by gateway

---
+++ src/main/java/org/gooru/nucleus/handlers/taxonomy/processors/utils/VersionValidationUtils.java
@@ -0,0 +1,35 @@
+package org.gooru.nucleus.handlers.taxonomy.processors.utils;
+
+import java.util.List;
+
+import org.gooru.nucleus.handlers.taxonomy.processors.exceptions.VersionDeprecatedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.MultiMap;
+
+/**
+ * @author ashish on 2/1/17.
+ */
+public final class VersionValidationUtils {
+
+    private static final String API_VERSION = "api.version";
+    private static final Logger LOGGER = LoggerFactory.getLogger(VersionValidationUtils.class);
+
+    private VersionValidationUtils() {
+        throw new AssertionError();
+    }
+
+    public static String validateVersion(List<String> deprecatedVersions, MultiMap messageHeaders) {
+        String currentVersion = findVersionFromMessageHeaders(messageHeaders);
+        if (currentVersion == null || deprecatedVersions.contains(currentVersion)) {
+            LOGGER.error("Version is not supported or null: {}", currentVersion);
+            throw new VersionDeprecatedException();
+        }
+        return currentVersion;
+    }
+
+    private static String findVersionFromMessageHeaders(MultiMap messageHeaders) {
+        return messageHeaders.get(API_VERSION);
+    }
+}
